5d3a932d148817ff0853e7e4acf0f391363e8372
Shelly_Wu <shellywu37@gmail.com>
2024-03-25 17:36:22 -0400

    get recipe from user id

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -15,12 +15,12 @@ const router = express.Router();
 
 router.route("/").get(getAllUsers).post(addUser);
 
-router.route("/:id").get(getUser);
+router.route("/:id").get(getUser).patch(updateUser);
 
-router.route("/:id/mealplan").get(getUserMealPlan);
+// router.route("/:id/mealplan").get(getUserMealPlan);
 
-router.route("/:id/mealplan/addrecipe").post(addRecipeToDate);
+// router.route("/:id/mealplan/addrecipe").post(addRecipeToDate);
 
-router.route("/:id/mealplan/:id").get(getMealPlanById).put(updateMealPlan);
+// router.route("/:id/mealplan/:id").get(getMealPlanById).put(updateMealPlan);
 
 export default router;
