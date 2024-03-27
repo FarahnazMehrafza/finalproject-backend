d00a74fd21c0464d0ac4248c2ef51d2cccbd2d2e
Farahnaz <161342559+FarahnazMehrafza@users.noreply.github.com>
2024-03-27 11:04:50 -0400

    back

--- src/models/recipeModel.ts
+++ src/models/recipeModel.ts
@@ -24,19 +24,10 @@ const recipeSchema = new Schema({
     type: String,
     required: true,
   },
-  ingredients: [
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: String,
-        required: true,
-        maxlength: 50,
-      },
-    },
-  ],
+  ingredients: {
+    type: String,
+    required: true,
+  },
   instructions: [
     {
       type: String,
@@ -61,7 +52,7 @@ const recipeSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    required: false,
   },
   mealPlan: {
     type: Schema.Types.ObjectId,
